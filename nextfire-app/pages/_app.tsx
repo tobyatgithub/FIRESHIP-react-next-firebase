c0d02b55122e3ee1bfa465560ce34fc5304d645a
tobyatgithub <toby.fangyuan@gmail.com>
2023-04-14 23:25:33 -0700

    USERS-AuthHook: use customized hook for user auth.

--- nextfire-app/pages/_app.tsx
+++ nextfire-app/pages/_app.tsx
@@ -3,10 +3,13 @@ import type { AppProps } from "next/app";
 import Navbar from "../components/Navbar";
 import { Toaster } from "react-hot-toast";
 import { UserContext } from "../lib/context";
+import { useUserData } from "../lib/hooks";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const userData = useUserData();
+
   return (
-    <UserContext.Provider value={{ user: {}, username: "Toby" }}>
+    <UserContext.Provider value={userData}>
       <Navbar />
       <Component {...pageProps} />
       <Toaster />
