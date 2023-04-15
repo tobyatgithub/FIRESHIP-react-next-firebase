f4ef9bd16114b881d991c143d1d5ea17aa995f6a
tobyatgithub <toby.fangyuan@gmail.com>
2023-04-14 22:42:28 -0700

    APP-TOAST: add toast example to the home page.

--- nextfire-app/pages/index.tsx
+++ nextfire-app/pages/index.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import Loader from "../components/Loader";
+import toast from "react-hot-toast";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -8,6 +9,7 @@ export default function Home() {
   return (
     <main>
       <h1>Sign Up</h1>
+      <button onClick={() => toast.success("hello toast!")}> Toast Me!</button>
       <div>
         <Loader show />
       </div>
