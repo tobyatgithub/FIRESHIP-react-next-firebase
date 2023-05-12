21edc44cd0a59343ce002d67695c5dbf2dce11ac
tobyatgithub <toby.fangyuan@gmail.com>
2023-05-12 00:22:37 -0700

    Add PostFeed, UserProfile, and [username]/index. Tested, works locally on mac mini.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # FIRESHIP-react-next-firebase
 Use react, next, and firebase to build a blog website that supports auth, server side rendering, and history data analysis.
+
+## Install
+
+## Run
+```bash
+cd nextfire-app
+npm run dev
+```
+## Features
--- nextfire-app/pages/[username]/index.tsx
+++ nextfire-app/pages/[username]/index.tsx
@@ -1,1 +1,23 @@
-export default (params) => {};
+import UserProfile from "../../components/UserProfile";
+import PostFeed from "../../components/PostFeed";
+
+// next will auto run this code when this page is requested
+export async function getServerSideProps({ query }) {
+  const { username } = query;
+
+  const userDoc = await getUserWithUsername(username);
+
+  return {
+    props: { user, posts },
+  };
+}
+
+export default function UserProfilePage({ user, posts }) {
+  return (
+    <main>
+      <h1>User Profile</h1>
+      <UserProfile user={user} />
+      <PostFeed posts={posts} />
+    </main>
+  );
+}
