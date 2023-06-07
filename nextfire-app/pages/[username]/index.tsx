0f20ad08ff3aa3efa66dcba183f9896ba7c9b3ed
tobyatgithub <toby.fangyuan@gmail.com>
2023-06-07 01:02:03 -0700

    SSR-4: srr and paginated home page feed worked.

--- nextfire-app/pages/[username]/index.tsx
+++ nextfire-app/pages/[username]/index.tsx
@@ -1,11 +1,27 @@
 import UserProfile from "../../components/UserProfile";
 import PostFeed from "../../components/PostFeed";
+import { getUserWithUsername, postToJSON } from "@/lib/firebase";
 
 // next will auto run this code when this page is requested
 export async function getServerSideProps({ query }) {
   const { username } = query;
 
   const userDoc = await getUserWithUsername(username);
+
+  // JSON serializable data
+  let user = null;
+  let posts = null;
+
+  if (userDoc) {
+    user = userDoc.data();
+    const postsQuery = userDoc.ref
+      .collection('posts')
+      .where('published', '==', true)
+      .orderBy('createdAt', 'desc')
+      .limit(5);
+
+      posts = (await postsQuery.get()).docs.map(postToJSON);
+  }
 
   return {
     props: { user, posts },
--- nextfire-app/pages/index.tsx
+++ nextfire-app/pages/index.tsx
@@ -1,20 +1,71 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import Loader from "../components/Loader";
+import PostFeed from "@/components/PostFeed";
+import Loader from "@/components/Loader";
+import { firestore, fromMillis, postToJSON } from "@/lib/firebase";
 import toast from "react-hot-toast";
-import Enter from "./enter";
 
-const inter = Inter({ subsets: ["latin"] });
+import { useState } from "react";
+// import { Inter } from "next/font/google";
+// const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
+const LIMIT = 1;
+
+export async function getServerSideProps(context){
+  const postsQuery = firestore
+    .collectionGroup('posts')
+    .where('published', '==', true)
+    .orderBy('createdAt', 'desc')
+    .limit(LIMIT);
+
+    const posts = (await postsQuery.get()).docs.map(postToJSON);
+
+    return {
+      props: {posts}, // will be passed to the page component as props
+    };
+}
+
+export default function Home(props) {
+  const [posts, setPosts] = useState(props.posts);
+  const [loading, setLoading] = useState(false);
+
+  const [postsEnd, setPostsEnd] = useState(false);
+
+  const getMorePosts = async () => {
+    if (posts.length == 0) {
+      setPostsEnd(true);
+      return;
+    }
+    setLoading(true);
+    const last = posts[posts.length - 1];
+    const cursor = typeof last?.createdAt === "number" ? fromMillis(last.createdAt) : last.createdAt;
+
+    const query = firestore
+      .collectionGroup('posts')
+      .where('published', '==', true)
+      .orderBy('createdAt', 'desc')
+      .startAfter(cursor)
+      .limit(LIMIT);
+
+    const newPosts = (await query.get()).docs.map((doc) => doc.data());
+
+    setPosts(posts.concat(newPosts));
+    setLoading(false);
+
+    if (newPosts.length < LIMIT) {
+      setPostsEnd(true);
+    }
+  }
+
   return (
     <main>
-      <h1>Sign Up</h1>
-      <button onClick={() => toast.success("hello toast!")}> Toast Me!</button>
-      <div>
+      <PostFeed posts={posts}/>
+      {!loading && !postsEnd && <button onClick={getMorePosts}>Load more</button>}
+      <Loader show={loading} />
+      {postsEnd && 'You have reached the end!'}
+      {/* <h1>Sign Up</h1> */}
+      {/* <button onClick={() => toast.success("hello toast!")}> Toast Me!</button> */}
+      {/* <div>
         <Loader show />
-      </div>
-      <Enter />
+      </div> */}
     </main>
   );
 }
