5729c10c6d19043bffb69745011adb2fbb5cbf9a
tobyatgithub <toby.fangyuan@gmail.com>
2023-06-08 15:52:49 -0700

    CRUS-3: add css for post, create editing form page for posts. allow markdown preview. fix register issue.

--- nextfire-app/pages/admin/[slug].tsx
+++ nextfire-app/pages/admin/[slug].tsx
@@ -1,10 +1,95 @@
+import styles from '../../styles/Admin.module.css';
 import MetaTags from "@/components/Metatags"
+import AuthCheck from '@/components/AuthCheck';
+import { firestore, auth, serverTimestamp } from '@/lib/firebase';
 
-export default function AdminPostEdit({}){
+import { useState } from 'react';
+import { useRouter } from 'next/router';
+
+import { useDocumentData } from 'react-firebase-hooks/firestore';
+import { useForm } from 'react-hook-form';
+import ReactMarkdown from 'react-markdown';
+import Link from 'next/link';
+import toast from 'react-hot-toast';
+
+export default function AdminPostEdit({ }) {
     return (
         <main>
-            <MetaTags title="Admin Page"/>
-            <h1>Edit Post</h1>
+            <MetaTags title="Admin Page" />
+            <AuthCheck>
+                <PostManager />
+            </AuthCheck>
         </main>
+    )
+}
+
+function PostManager() {
+    const [preview, setPreview] = useState(false);
+
+    const router = useRouter();
+    const { slug } = router.query; 
+
+    const postRef = firestore.collection('users').doc(auth.currentUser.uid).collection('posts').doc(slug);
+    const [post] = useDocumentData(postRef); // listen to the post in real time
+    // const [post] = useDocumentDataOnce(postRef); // listen to the post in only once might make more sense here.
+
+    return (
+        <main className={styles.container}>
+            {post && (
+                <>
+                    <section>
+                        <h1>{post.title}</h1>
+                        <p>ID: {post.slug}</p>
+
+                        <PostForm postRef={postRef} defaultValues={post} preview={preview} />
+                    </section>
+
+                    <aside>
+                        <h3>Tools</h3>
+                        <button onClick={() => setPreview(!preview)}>{preview ? 'Edit' : 'Preview'}</button>
+                        <Link href={`/${post.username}/${post.slug}`}>
+                            <button className='btn-blue'>Live view</button>
+                        </Link>
+                    </aside>
+                </>)}
+        </main>
+    );
+}
+
+function PostForm({ defaultValues, postRef, preview }) {
+    // watch: will treat the thing as a state, and will update the thing when it changes.
+    const { register, handleSubmit, reset, watch } = useForm({ defaultValues, mode: 'onChange' });
+    // register is used to connect user input with form.
+    const updatePost = async ({ content, published }) => {
+        await postRef.update({
+            content,
+            published,
+            updatedAt: serverTimestamp(),
+        });
+
+        reset({ content, published });
+        toast.success('Post updated successfully!');
+    };
+
+    console.log("toby, styles = ", styles);
+    
+    return (
+        <form onSubmit={handleSubmit(updatePost)}>
+            {preview && (
+                <div className="card">
+                    <ReactMarkdown>{watch('content')}</ReactMarkdown>
+                </div>
+            )}
+
+            <div className={preview ? styles.hidden : styles.controls}>
+                <textarea name="content" {...register("content", {required: "Required",})}></textarea>
+                <fieldset>
+                    <input className={styles.checkbox} name="published" type="checkbox" {...register("published", {required: "Required"})} />
+                    <label>Published</label>
+                </fieldset>
+
+                <button type="submit" className='btn-green'>Save Changes</button>
+            </div>
+        </form>
     )
 }
