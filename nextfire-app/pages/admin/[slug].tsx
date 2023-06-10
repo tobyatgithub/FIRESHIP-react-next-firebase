1889ea505d3cfa6a133ce54c0392b8822f77bf2c
tobyatgithub <toby.fangyuan@gmail.com>
2023-06-10 15:13:01 -0700

    CRUD-5: image uploads
    Added component to upload image in post write... hmmmm
    it works but in a somewhat naive way.
    Notice you need to enable firestore storage to make it work for
    the first time.

--- nextfire-app/pages/admin/[slug].tsx
+++ nextfire-app/pages/admin/[slug].tsx
@@ -2,6 +2,7 @@ import styles from '../../styles/Admin.module.css';
 import MetaTags from "@/components/Metatags"
 import AuthCheck from '@/components/AuthCheck';
 import { firestore, auth, serverTimestamp } from '@/lib/firebase';
+import ImageUploader from '@/components/ImageUploader';
 
 import { useState } from 'react';
 import { useRouter } from 'next/router';
@@ -84,6 +85,7 @@ function PostForm({ defaultValues, postRef, preview }) {
             )}
 
             <div className={preview ? styles.hidden : styles.controls}>
+                <ImageUploader/>
                 <textarea {...register("content", {
                 // <textarea name="content" {...register("content", {
                     required: { value: true, message: "content is required" },
