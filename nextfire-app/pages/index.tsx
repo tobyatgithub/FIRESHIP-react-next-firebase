c3dbbbcb3474a7632d580ce54d9034922fdbc21c
tobyatgithub <toby.fangyuan@gmail.com>
2023-06-10 14:23:24 -0700

    BUGFIX: fix the issue of no posts were shown.
    Turns out that there was a typo in the post data creation. 'cratedAt'
    shall have been 'createdAt'.
    Debug code to be removed.

--- nextfire-app/pages/index.tsx
+++ nextfire-app/pages/index.tsx
@@ -8,23 +8,38 @@ import { useState } from "react";
 // import { Inter } from "next/font/google";
 // const inter = Inter({ subsets: ["latin"] });
 
-const LIMIT = 1;
+const LIMIT = 5;
 
-export async function getServerSideProps(context){
+export async function getServerSideProps(context) {
   const postsQuery = firestore
     .collectionGroup('posts')
     .where('published', '==', true)
     .orderBy('createdAt', 'desc')
     .limit(LIMIT);
 
-    const posts = (await postsQuery.get()).docs.map(postToJSON);
+  const posts = (await postsQuery.get()).docs.map(postToJSON);
 
-    return {
-      props: {posts}, // will be passed to the page component as props
-    };
+  return {
+    props: { posts }, // will be passed to the page component as props
+  };
+}
+
+async function test() {
+  // const postsQuery = firestore
+  //   .collection('posts')
+  //   .where('published', '==', true)
+  //   .orderBy('createdAt', 'desc')
+  //   .limit(LIMIT);
+  const postsQuery = firestore.collectionGroup('posts').where('published', '==', true);
+  const posts2 = (await postsQuery.get()).docs.map(postToJSON);
+  console.log("toby2, posts2 = ", posts2);
 }
 
 export default function Home(props) {
+  console.log("toby, props = ", props);
+
+  test();
+
   const [posts, setPosts] = useState(props.posts);
   const [loading, setLoading] = useState(false);
 
@@ -61,7 +76,7 @@ export default function Home(props) {
   return (
     <main>
       <MetaTags title="Home" description="Home page" />
-      <PostFeed posts={posts}/>
+      <PostFeed posts={posts} />
       {!loading && !postsEnd && <button onClick={getMorePosts}>Load more</button>}
       <Loader show={loading} />
       {postsEnd && 'You have reached the end!'}
