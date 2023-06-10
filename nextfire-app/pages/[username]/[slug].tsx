93fc8e7ed2aad32b39cf022b5a88b53ca788bfd7
tobyatgithub <toby.fangyuan@gmail.com>
2023-06-10 15:51:32 -0700

    HEARTS: add component for heart button and update heart counts.

--- nextfire-app/pages/[username]/[slug].tsx
+++ nextfire-app/pages/[username]/[slug].tsx
@@ -2,6 +2,9 @@ import styles from '../../styles/Post.module.css';
 import { firestore, getUserWithUsername, postToJSON } from "@/lib/firebase";
 import { useDocumentData } from 'react-firebase-hooks/firestore';
 import PostContent from '@/components/PostContent';
+import HeartButton from '@/components/HeartButton';
+import AuthCheck from '@/components/AuthCheck';
+import Link from 'next/link';
 
 export async function getStaticProps({ params }) {
   const { username, slug } = params;
@@ -69,7 +72,13 @@ export default function Post(props) {
         <p>
           <strong>{post.heartCount || 0} 🤍</strong>
         </p>
-
+        <AuthCheck fallback={
+          <Link href="/enter">
+            <button>Sign Up</button>
+          </Link>
+        }>
+          <HeartButton postRef={postRef}/>
+        </AuthCheck>
       </aside>
     </main>
   );
